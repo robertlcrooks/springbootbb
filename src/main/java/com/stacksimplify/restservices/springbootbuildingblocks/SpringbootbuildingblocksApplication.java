9fc9b1e891e283d7a23f91ed5d716f2ca90fb2e8
Robert Crooks <robertcrooks@matrix-2.local>
2021-04-12 19:20:27 -0400

    First Commit

---
+++ src/main/java/com/stacksimplify/restservices/springbootbuildingblocks/SpringbootbuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices.springbootbuildingblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbuildingblocksApplication.class, args);
+	}
+
+}
